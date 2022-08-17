959da4273e7401c14be44786276717d54ac16f18
Sara Echeverría <84192821+bl33h@users.noreply.github.com>
2022-08-17 16:14:56 -0600

    menu placed.

---
+++ MathFunctions.py
@@ -0,0 +1,26 @@
+#/**
+ #* *Copyright (C), 2022-2023, Sara Echeverria (bl33h)
+    # *@author Sara Echeverria
+    # *FileName: Math Functions
+    # @version: I
+    #- Creation: 01/08/2022
+    #- Last modification: 17/08/2022
+
+# Boolean       
+keepGoing = True
+
+# While cycle for the menu
+while keepGoing:
+    print("--- What would you like to do? ---")
+    print("1. Spread function")
+    print("2. Pseudorandom number generator")
+    
+    option = input("\nType the number that corresponds to the option you wanna execute: ")
+    
+    # First option || Spread function & hash
+    if option == "1":
+        print("\n")
+    if option == "2":
+        print("\n")
+    else:
+        print("\nERROR: Please type a valid option.")
